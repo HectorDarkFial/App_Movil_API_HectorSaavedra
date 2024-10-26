102780c468bdd17508b135692da6559c3405f22a
Hector-Saavedra <105993709+HectorDarkFial@users.noreply.github.com>
2024-10-26 14:56:40 -0300

    Add files via upload

---
+++ HectorSaavedra21205095-4/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'HectorSaavedra21205095-4',
+  webDir: 'www'
+};
+
+export default config;
